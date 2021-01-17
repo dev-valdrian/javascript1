0b207fe1bb37bc5e11a9b19466ba1ab241dd2447
dev-valdrian <valdrianopromo@gmail.com>
2021-01-17 16:02:17 -0400

    Primeira Aula de Introdução

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var nome = "Valdriano Evangelista";
+var idade =35;
+var idade2 = 36;
+var frase = "Japão é a melhor seleção do mundo";
+//alert("Bem-vindo "+nome);
+//alert(idade+idade2);
+console.log(nome);
+console.log(idade+idade2);
+console.log(frase.replace("Japão","Brasil"));//Replace troca o termo selecionado
+alert(frase.replace("Japão","Brasil"));
+//console.log(frase.toUpperCase());//uppercase deixa tudo maiusculo
+//console.log(frase.toLowerCase());//lowercase deixa tudo minusculo
