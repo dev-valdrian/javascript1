e8f33e7ebc82966874f77a9a8c5be19e42c5921b
dev-valdrian <valdrianopromo@gmail.com>
2021-01-17 16:36:17 -0400

    Primeira Aula de Introdução 2

--- js/main.js
+++ js/main.js
@@ -1,12 +1,24 @@
-var nome = "Valdriano Evangelista";
-var idade =35;
-var idade2 = 36;
-var frase = "Japão é a melhor seleção do mundo";
+var lista = ["maça", "pera", "uva", "salada mista"];
+
+
+//alert(lista[0]);
+//lista.push("abacaxi");//push coloca itens
+//lista.pop(); // pop retira itens
+//console.log(lista);
+//console.log(lista.length); // conta elementos na lista
+//console.log(lista.reverse()); // inverte a ordem dos elementos
+//console.log(lista.toString());
+//console.log(lista.join( " - " ));
+
+//var nome = "Valdriano Evangelista";
+//var idade =35;
+//var idade2 = 36;
+//var frase = "Japão é a melhor seleção do mundo";
 //alert("Bem-vindo "+nome);
 //alert(idade+idade2);
-console.log(nome);
-console.log(idade+idade2);
-console.log(frase.replace("Japão","Brasil"));//Replace troca o termo selecionado
-alert(frase.replace("Japão","Brasil"));
+//console.log(nome);
+//console.log(idade+idade2);
+//console.log(frase.replace("Japão","Brasil"));//Replace troca o termo selecionado
+//alert(frase.replace("Japão","Brasil"));
 //console.log(frase.toUpperCase());//uppercase deixa tudo maiusculo
 //console.log(frase.toLowerCase());//lowercase deixa tudo minusculo
